77636a5eb65fc9e45633914c173b991e18c009c0
Michael Bach <migobach@gmail.com>
2018-06-23 11:41:54 -0600

    unable to get JSX elements to agree, moving on to search

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -8,6 +8,7 @@ class Beers extends Component {
   state = { beers: [], hasMore: true, nextHref: null }
 
   LoadMore = (page) => { 
+    debugger
     const self = this;
     let url = axios.baseUrl + '/beers';
       if(this.state.nextHref) 
@@ -17,51 +18,58 @@ class Beers extends Component {
   componentDidMount() {
     axios.get('http://localhost:5100/api/all_beers?page=1&per_page=6', { cache: true }) 
       .then(res => {
-        this.setState({ beers: res.data.entries })
-      }) 
-      .catch( error => {
-        debugger
-        console.log(error.response);
-    });
+      if(res.next_href) {
+          this.setState({ beers: res.data.entries })
+                nextHref: res.next_href
+        } else {
+            this.setState({
+                hasMore: false
+            });
+        }
+      // .catch( error => {
+      //   debugger
+      //   console.log(error.response);
+      // });
+      })
   }
 
 
   render() {
+    const loader = <Segment className="loader"> Loading ...</Segment>
+
+      // var items = [];
+    <Segment>
+      <Card.Group>
+        { this.state.beers.map( beer =>
+          <Card 
+          key={beer.id}
+          >
+            <Card.Content>{beer.name_display}</Card.Content>
+            <Card.Meta>{beer.description}</Card.Meta>
+            <Card.Content extra>
+            <Icon name="beer" /><p>IBUs:{beer.ibu} | ABV:{beer.abv}</p>
+            </Card.Content>
+          </Card>
+          ) 
+        }
+      </Card.Group>
+    </Segment>
+
+
+
     return (
-      
       <Segment>
       <InfiniteScroll
            pageStart={0}
            loadMore={this.loadMore} 
            hasMore={true || false}
-           loader={<div className="loader" key={0}>Loading ...</div>}
-       >
+           loader={loader}
+           >
           <Header as='h1' textAlign='center'>Top 50 Beers</Header>
 
-        <Card.Group>
-            { this.state.beers.map( beer =>
-              <Card 
-              key={beer.id}
-              >
-                <Card.Content>{beer.name_display}</Card.Content>
-                <Card.Meta>{beer.description}</Card.Meta>
-                <Card.Content extra>
-                <Icon name="beer" /><p>IBUs:{beer.ibu} | ABV:{beer.abv}</p>
-                </Card.Content>
-              </Card>
-              )
-            }
-        </Card.Group>
-            </InfiniteScroll>
-            {/* <Pagination
-              defaultActivePage={1}
-              firstItem={null}
-              lastItem={null}
-              pointing
-              secondary
-              totalPages={5}
-            /> */}
-      </Segment>
+       
+       </InfiniteScroll>
+       </Segment>
     )
   }
 }
